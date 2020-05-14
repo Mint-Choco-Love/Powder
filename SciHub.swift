1cb5d7010678f35f87c0eff2ed3d49c5420cba7c
Mint-Choco-Love <miraak@naver.com>
2020-05-14 14:16:58 +0900

    simple

--- SciHub.swift
+++ SciHub.swift
@@ -1,8 +1,8 @@
 
 struct SciHub {
     static func doi(url: String) {
-        // let test = "https://sci-hub.tw/https://doi.org/10.1145/253769.253798"
-        let doiURL = "https://sci-hub.tw/https://doi.org/" + url
+        // let test = "https://sci-hub.tw/10.1145/253769.253798"
+        let doiURL = "https://sci-hub.tw/" + url
         let htmlString = Crawler().getHTML(url: doiURL)
         let tags = Util.getImgURLs(htmlString ?? "")
         ImageExtractor().extract(urls: tags)
